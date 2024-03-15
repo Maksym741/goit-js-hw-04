c6316de9c06a7bef9f2c627a866e51c7c7a8a666
Maksym741 <pastushinamaksim@gmail.com>
2024-03-15 19:41:58 +0100

    .

--- js/task-1.js
+++ js/task-1.js
@@ -8,8 +8,6 @@ function isEnoughCapacity(products, containerSize) {
     return enoughCapacity <= containerSize ? true : false;
 }
 
-
-
 //** Напиши функцію isEnoughCapacity(products, containerSize), яка обчислює,
 //** чи помістяться всі товари в контейнер при пакуванні.
 
@@ -23,21 +21,15 @@ function isEnoughCapacity(products, containerSize) {
 //** Функція має повернути результат перевірки, чи помістяться всі товари в контейнер.
 //** Тобто порахувати загальну кількість товарів в об’єкті products і повернути true,
 //** якщо вона менше або дорівнює containerSize, і false, якщо ні.
-
-
-
 console.log(
     isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
   ); // true
-
 console.log(
     isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
   ); // false
-
 console.log(
     isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
   ); // true
-
 console.log(
     isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
   ); // false
